350bb83c51414bc85986f9731927962e9c6d8615
yoker300 <elilaufer@gmail.com>
2025-12-01 20:53:23 +0200

    First commit

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'smartlist-v2';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
